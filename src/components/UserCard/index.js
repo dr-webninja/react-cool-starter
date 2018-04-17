c3e92187e4897731173d7694a2daaff2717f9e47
danielreis <dr.webninja@gmail.com>
2018-04-17 22:10:31 +0100

    Test image loading from img src; Add custom font; Add script to update all snapshots (for all failing tests)

--- src/components/UserCard/index.js
+++ src/components/UserCard/index.js
@@ -4,11 +4,19 @@ import React from 'react';
 
 import styles from './styles.scss';
 
+import small from '../../app/assets/small-image.png';
+import medium from '../../app/assets/medium-image.jpg';
+import large from '../../app/assets/large-image.png';
+
 type Props = { info: Object };
 
 export default ({ info }: Props) => (
   <div className={styles.UserCard}>
     <h4>User Card</h4>
+    <img src={small} alt="Logo" role="presentation" className={styles.image} />
+    <img src={medium} alt="Logo" role="presentation" className={styles.image} />
+    <img src={large} alt="Logo" role="presentation" className={styles.image} />
+    <p>OLA</p>
     <ul>
       <li>Name: {info.name}</li>
       <li>Phone: {info.phone}</li>
